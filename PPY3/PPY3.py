f92d0a504da4e37303eb4027207fcdc0cfa517c5
Zheckan <Zheckan1120@gmail.com>
2024-03-18 12:48:16 +0100

    Task 3. Created a class SquareGenerator

--- PPY3/PPY3.py
+++ PPY3/PPY3.py
@@ -7,3 +7,14 @@ def squares(start, end):
     return [i**2 for i in range(start, end+1)]
 
 print(squares(1,10))
+
+# 3
+class SquareGenerator:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def squares(self):
+        return [i**2 for i in range(self.start, self.end+1)]
+
+print(SquareGenerator(1,10).squares())
